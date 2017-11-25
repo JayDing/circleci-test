1ba395f599843964a6259315038a7f9fe73b8f72
locker_rj45 <s124373723@gmail.com>
2017-11-25 22:35:06 +0800

    fix test/test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ describe('Array', function() {
   describe('#indexOf()', function() {
     it('不存在陣列裡時 回傳 -1', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
-      assert.equal(1, [1,2,3].indexOf(5566));
+      assert.equal(-1, [1,2,3].indexOf(5566));
     });
   });
 });
