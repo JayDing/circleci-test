af77ddbf49816b28af131d504c090aad1b32c11e
Jay Ding <Jay Ding>
2017-11-25 15:21:55 +0800

    first commit

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('不存在陣列裡時 回傳 -1', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
